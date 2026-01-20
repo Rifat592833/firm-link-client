14acccacb9f2f5f73858feb5c4d2e5bbf6a247c3
unknown <mahmudulhasanrifat79@gmaile.com>
2026-01-21 02:03:37 +0600

    Fix GitHub Pages routing with HashRouter

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,16 +1,16 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import RootLayout from "../Layouts/RootLayout";
 import Home from "../Pages/Home/Home/Home";
 import AboutUs from "../Pages/AboutUs/AboutUs";
 import Coverage from "../Pages/Coverage/Coverage";
 import AuthLayout from "../Layouts/AuthLayout";
-import Login from "../Pages/Authentication/Login/login";
 import Register from "../Pages/Authentication/Register/Register";
 import FarmerRegister from "../Pages/Authentication/FarmerRegister/FarmerRegister";
 import FarmerLogin from "../Pages/Authentication/FarmerLogin/FarmerLogin";
+import Login from "../Pages/Authentication/Login/Login";
 
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     Component:RootLayout,
